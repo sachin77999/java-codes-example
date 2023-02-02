6e43b3dd65c3184ae754c968c38765a33c749982
sachin77999 <sachinsoni77999@gmail.com>
2023-02-02 11:32:50 +0530

    updates

---
+++ src/ArrayList/JavaProgramDoubleToString.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class JavaProgramDoubleToString {
+    public static void main(String args[])
+    {
+        double number = 123.456;
+        String output = String.valueOf(number);
+        System.out.println(output);
+    }
+}
